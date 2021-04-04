3c11713b2307e6c1e555f04326d541160ea45951
cayter <cayter@appist.io>
2021-04-05 00:49:26 +0800

    docs: add story for `Intro` component

---
+++ stories/Intro.stories.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { Meta, Story } from "@storybook/addon-svelte-csf";
+  import Intro from "../src/Intro.svelte";
+</script>
+
+<Meta title="Components/Intros" component={Intro} />
+
+<Story name="Intros">
+  <Intro
+    class="max-w-6xl"
+    shortDesc="A better way to ship apps"
+    longDesc="Start your project with a SQL/NoSQL database,
+authentication, background processing, realtime subscriptions, serverless APIs, and storage, all
+in less than 5 minutes."
+    images={[{ alt: "Introduction", src: "https://i.imgur.com/I8hzGS5.png" }]}
+    links={[
+      {
+        color: "primary",
+        href: "/register",
+        name: "Register Now",
+        outline: false,
+      },
+    ]}
+  />
+</Story>
